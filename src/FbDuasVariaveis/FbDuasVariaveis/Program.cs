caecffd278dc5d281e0c8285a8de9e46c96486ee
naiaragabriela <naigabi31@gmail.com>
2023-03-05 22:11:37 -0300

    sequencia de fibonacci usando função

---
+++ src/FbDuasVariaveis/FbDuasVariaveis/Program.cs
@@ -0,0 +1,21 @@
+﻿int anterior, proximo, posicao;
+
+int NumeroDigitado()
+{
+    Console.Write("Informe até qual posição da sequência deseja ver: ");
+    return int.Parse(Console.ReadLine());
+}
+posicao = NumeroDigitado();
+anterior = 1;
+proximo = 0;
+
+void ResultadoFb()
+{
+    for (int i = 1; i<= posicao; i++)
+    {
+        Console.Write(proximo + anterior + ",");
+        proximo += anterior;
+        anterior = (proximo - anterior);
+    }
+}
+ResultadoFb();
